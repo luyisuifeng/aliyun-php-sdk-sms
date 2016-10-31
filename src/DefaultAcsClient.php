6fc30550074f4b452674256da1553546d7d5cba5
luyisuifeng <1105549812@qq.com>
2016-10-31 17:07:00 +0800

    Update DefaultAcsClient.php
     修正Exception命名空间问题

--- src/DefaultAcsClient.php
+++ src/DefaultAcsClient.php
@@ -18,8 +18,8 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-use Aliyun\Core\Exceptions\ClientException;
-use Aliyun\Core\Exceptions\ServerException;
+use Aliyun\Core\Exception\ClientException;
+use Aliyun\Core\Exception\ServerException;
 use Aliyun\Core\Http\HttpHelper;
 use Aliyun\Core\Profile\DefaultProfile;
 use Aliyun\Core\Regions\EndpointProvider;
